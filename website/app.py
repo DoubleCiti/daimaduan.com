d504bd3e3e2ee3bbfc721ff55c4bdda942002afc
David Xie <david.scriptfan@gmail.com>
2015-09-11 17:28:52 +0800

    Add error page for 404 and 500

--- website/app.py
+++ website/app.py
@@ -1,6 +1,7 @@
 #-*-encoding:utf-8-*-
 from bootstrap import app
 from bottle import run, request, redirect
+from bottle import error
 from bottle import jinja2_view
 from bottle import static_file
 
@@ -12,6 +13,19 @@ from models import Paste
 
 from forms import SignupForm
 from forms import SigninForm
+
+
+@error(404)
+@jinja2_view('error.html')
+def error_404(error):
+    return {'title': u"页面找不到", 'message': u"您所访问的页面不存在!"}
+
+
+@error(500)
+@jinja2_view('error.html')
+def error_500(error):
+    return {'title': u"服务器错误", 'message': u"服务器开小差了, 晚点再来吧!"}
+
 
 @app.route('/')
 @jinja2_view('index.html')
