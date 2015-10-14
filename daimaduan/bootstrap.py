8c1468d27060b25f16b1a3358230ae11b9218937
greatghoul <greatghoul@gmail.com>
2015-10-15 02:51:55 +0800

    add comment

--- daimaduan/bootstrap.py
+++ daimaduan/bootstrap.py
@@ -6,6 +6,7 @@ from daimaduan.jinja_ext import JinajaPlugin
 
 app = bottle.default_app()
 
+# Auto cast `site.debug` to boolean type.
 app.config.meta_set('site.debug', 'filter', bool)
 app.config.load_config('config.cfg')
 app.config['SECRET_KEY'] = app.config['site.secret_key']
