0e49b065f47ce7566dbfe5bd4f4011a4705504c0
greatghoul <greatghoul@gmail.com>
2015-10-15 02:34:01 +0800

    fix pastes scripts
    allow to add more that two codes

--- daimaduan/static/js/pastes.js
+++ daimaduan/static/js/pastes.js
@@ -1,10 +1,20 @@
-$(document).ready(function() {
-  var i = 1;
-  var code = $('.code').last().clone();
-  $('#one_more').click(function(event) {
-    event.preventDefault();
-    $('#codes').append($(code));
-    $('.code').last().find('input').first().attr('name', 'codes-' + i + '-title');
-    $('.code').last().find('textarea').first().attr('name', 'codes-' + i + '-content');
+(function() {
+  function initCodes() {
+    var i = 1;
+    
+
+    $(document).on('click', '#one_more', function(event) {
+      event.preventDefault();
+
+      var $code = $('.code').last().clone();
+      $code.find('input').first().attr('name', 'codes-' + i + '-title');
+      $code.find('textarea').first().attr('name', 'codes-' + i + '-content');
+
+      $code.appendTo('#codes');
+    });
+  }
+
+  $(document).ready(function() {
+    initCodes();
   });
-});
+})();
