d75126ecf313717a093c45310ac1313081172f17
greatghoul <greatghoul@gmail.com>
2015-10-16 05:46:47 +0800

    use absolute template path

--- daimaduan/bootstrap.py
+++ daimaduan/bootstrap.py
@@ -5,10 +5,11 @@ import os.path
 from bottle_login import LoginPlugin
 from daimaduan.jinja_ext import JinajaPlugin
 
+app_root = os.path.dirname(__file__)
 app = bottle.default_app()
 
 # Auto cast `site.debug` to boolean type.
-config_file = os.path.join(os.path.dirname(__file__), 'config.cfg')
+config_file = os.path.join(app_root, 'config.cfg')
 app.config.meta_set('site.debug', 'filter', bool)
 app.config.load_config(config_file)
 app.config['SECRET_KEY'] = app.config['site.secret_key']
@@ -16,5 +17,8 @@ app.config['SECRET_KEY'] = app.config['site.secret_key']
 # Setup bottle debug mode through config `site.debug`
 bottle.debug(app.config['site.debug'])
 
-jinja = app.install(JinajaPlugin())
-login = app.install(LoginPlugin())
+jinja = JinajaPlugin(template_path=os.path.join(app_root, 'templates'))
+login = LoginPlugin()
+
+app.install(login)
+app.install(jinja)
