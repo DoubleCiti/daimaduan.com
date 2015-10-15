000694c9e16cc4965008c3c21df7a9f4f05f837c
greatghoul <greatghoul@gmail.com>
2015-10-16 05:30:56 +0800

    load config using the absolute path

--- daimaduan/bootstrap.py
+++ daimaduan/bootstrap.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import bottle
+import os.path
 
 from bottle_login import LoginPlugin
 from daimaduan.jinja_ext import JinajaPlugin
@@ -7,8 +8,9 @@ from daimaduan.jinja_ext import JinajaPlugin
 app = bottle.default_app()
 
 # Auto cast `site.debug` to boolean type.
+config_file = os.path.join(os.path.dirname(__file__), 'config.cfg')
 app.config.meta_set('site.debug', 'filter', bool)
-app.config.load_config('config.cfg')
+app.config.load_config(config_file)
 app.config['SECRET_KEY'] = app.config['site.secret_key']
 
 # Setup bottle debug mode through config `site.debug`
