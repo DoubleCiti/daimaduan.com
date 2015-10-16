d17f5394aaef45464fb5cfbc299ec0fde76c6ebc
David Xie <david.scriptfan@gmail.com>
2015-10-16 23:11:19 +0800

    Add tox and pep8 support

--- daimaduan/runserver.py
+++ daimaduan/runserver.py
@@ -11,6 +11,5 @@ if __name__ == '__main__':
         return static_file(filepath, root='./static')
 
     run(application, host='0.0.0.0',
-                     server='paste',
                      port=8080,
                      reloader=True)
