0d3e9afab3fa3179e73e1bb69a16f7e0597fe83b
greatghoul <greatghoul@gmail.com>
2015-10-22 22:45:31 +0800

    修正 setup.py install requires 的问题
    使用 -e 解决

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import sys
+import re
 import subprocess
 from setuptools import setup, find_packages
 
 # get requirements
+install_requires = []
+dependency_links = []
 with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
+    for line in f.read().splitlines():
+        if line.startswith('-e'):
+            install_requires.append(re.sub(r'.*egg=', '', line))
+            dependency_links.append(re.sub(r'^-e ', '', line))
+        else:
+            install_requires.append(line)
 
 # get dev version from git
 cmd = subprocess.Popen("git log --oneline | head -1 | awk '{print $1}'", shell=True, stdout=subprocess.PIPE)
@@ -25,5 +32,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=required
+    install_requires=install_requires,
+    dependency_links=dependency_links
 )
