decb61555b532bfe622529a8dffc2153d89c9807
David Xie <david.scriptfan@gmail.com>
2015-10-25 21:10:20 +0800

    去掉了bootstrap中的app_root变量, 这个值导致部署到服务器上后, 找不到对应的资源文件

--- daimaduan/bootstrap.py
+++ daimaduan/bootstrap.py
@@ -7,7 +7,6 @@ from bottle_login import LoginPlugin
 from daimaduan.jinja_ext import JinajaPlugin
 from rauth import OAuth2Service
 
-app_root = os.path.dirname(__file__)
 app = bottle.default_app()
 
 # Setup logger
@@ -16,13 +15,13 @@ logger = logging.getLogger('daimaduan')
 
 # Auto cast `site.debug` to boolean type.
 app.config.meta_set('site.debug', 'filter', bool)
-app.config.load_config(os.path.join(app_root, 'config.cfg'))
+app.config.load_config('config.cfg')
 app.config['SECRET_KEY'] = app.config['site.secret_key']
 
 # Setup bottle debug mode through config `site.debug`
 bottle.debug(app.config['site.debug'])
 
-jinja = JinajaPlugin(template_path=os.path.join(app_root, 'templates'))
+jinja = JinajaPlugin(template_path='templates')
 login = LoginPlugin()
 
 app.install(login)
