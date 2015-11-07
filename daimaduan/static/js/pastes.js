e39a39bdd3e925f99870af73ec0e0168c308bff7
David Xie <david.scriptfan@gmail.com>
2015-11-07 22:28:13 +0800

    修复了#53

--- daimaduan/static/js/pastes.js
+++ daimaduan/static/js/pastes.js
@@ -1,15 +1,18 @@
 (function() {
   function initCodes() {
     var i = 1;
-    
 
     $(document).on('click', '#one_more', function(event) {
       event.preventDefault();
 
       var $code = $('.code').last().clone();
       $code.find('input').first().attr('name', 'codes-' + i + '-title');
+      $code.find('input').first().val('');
+      $code.find('select').first().attr('name', 'codes-' + i + '-tag');
       $code.find('textarea').first().attr('name', 'codes-' + i + '-content');
+      $code.find('textarea').first().val('');
 
+      i += 1;
       $code.appendTo('#codes');
     });
   }
