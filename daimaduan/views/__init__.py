f94400bbcc5b18dd71558d979923c6bbf1a51426
David Xie <david.scriptfan@gmail.com>
2015-12-20 23:06:24 +0800

    加入一个简单的页面来显示当前网站的状态

--- daimaduan/views/__init__.py
+++ daimaduan/views/__init__.py
@@ -1,9 +1,12 @@
 from bottle import request
+from bottle import jinja2_view
 from bottle import Jinja2Template
 
 from daimaduan.bootstrap import app
 from daimaduan.bootstrap import login
+from daimaduan.models import Code
 from daimaduan.models import User
+from daimaduan.models import Paste
 
 import daimaduan.views.errors
 import daimaduan.views.pastes
@@ -23,3 +26,11 @@ def before_request():
 @app.hook('after_request')
 def after_request():
     Jinja2Template.defaults['session'] = request.environ.get('beaker.session')
+
+
+@app.get('/status')
+@jinja2_view('status.html')
+def status():
+    return {'pastes_count': Paste.objects().count(),
+            'codes_count': Code.objects().count(),
+            'users_count': User.objects().count()}
