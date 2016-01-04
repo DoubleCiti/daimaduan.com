66b50a22810e98ca7cda86a527434beb5f7e90a5
greatghoul <greatghoul@gmail.com>
2016-01-04 14:28:33 +0800

    嵌入的代码加上标题样式

--- daimaduan/templates/pastes/embed.js
+++ daimaduan/templates/pastes/embed.js
@@ -1,4 +1,7 @@
 document.write('<link rel="stylesheet" href="http://{{ config['site.domain'] }}/static/css/embed.css" />');
 {% for code in paste.codes -%}
+document.write('<div class="daimaduan-code" id="paste-{{ paste.hash_id }}">');
+document.write('<div class="daimaduan-code-block">');
 document.write('{{ code.highlight_content | replace('\n', '\\n') | replace('\r', '') | safe }}');
+document.write('</div><div class="daimaduan-code-meta">{{ code.title }}</div></div>');
 {% endfor -%}
