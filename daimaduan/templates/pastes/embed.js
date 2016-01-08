e6a41200b9f3f313591f733d0bb8e4c959ac4beb
greatghoul <greatghoul@gmail.com>
2016-01-08 21:32:25 +0800

    code 上添加嵌入代码的引用地址及代码段首页链接

--- daimaduan/templates/pastes/embed.js
+++ daimaduan/templates/pastes/embed.js
@@ -3,5 +3,5 @@ document.write('<link rel="stylesheet" href="http://{{ config['site.domain'] }}/
 document.write('<div class="daimaduan-code" id="paste-{{ paste.hash_id }}">');
 document.write('<div class="daimaduan-code-block">');
 document.write('{{ code.highlight_content | replace('\n', '\\n') | replace('\r', '') | safe }}');
-document.write('</div><div class="daimaduan-code-meta">{{ code.title }}</div></div>');
+document.write('</div><div class="daimaduan-code-meta"><a href="http://{{ config['site.domain'] }}" style="float: right">代码段</a><a href="http://{{ config['site.domain'] }}/paste/{{ paste.hash_id }}#code-{{ code.hash_id }}">{{ code.title }}</a></div></div>');
 {% endfor -%}
