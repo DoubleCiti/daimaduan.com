41cd1d4fc3f021a4388e108853f8ba1bcfb7cea2
greatghoul <greatghoul@gmail.com>
2016-01-08 22:36:09 +0800

    更新代码段嵌入脚本的样式

--- daimaduan/templates/pastes/embed.js
+++ daimaduan/templates/pastes/embed.js
@@ -1,7 +1,8 @@
-document.write('<link rel="stylesheet" href="http://{{ config['site.domain'] }}/static/css/embed.css" />');
+{% set root_url = url_for() -%}
+document.write('<link rel="stylesheet" href="{{ root_url }}/static/css/embed.css" />');
 {% for code in paste.codes -%}
 document.write('<div class="daimaduan-code" id="paste-{{ paste.hash_id }}">');
 document.write('<div class="daimaduan-code-block">');
 document.write('{{ code.highlight_content | replace('\n', '\\n') | replace('\r', '') | safe }}');
-document.write('</div><div class="daimaduan-code-meta"><a href="http://{{ config['site.domain'] }}" style="float: right">代码段</a><a href="http://{{ config['site.domain'] }}/paste/{{ paste.hash_id }}#code-{{ code.hash_id }}">{{ code.title }}</a></div></div>');
+document.write('</div><div class="daimaduan-code-meta"><div class="daimaduan-copyright">powered by <a href="{{ root_url }}"><strong>代码段</strong></a></div><div class="daimaduan-code-name"><a href="{{ root_url }}/paste/{{ paste.hash_id }}#code-{{ code.hash_id }}">{{ code.title * 10 }}</a></div></div></div>');
 {% endfor -%}
