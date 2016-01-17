01c19a2a0106d5724562e2c5d4a0eebc816740b6
David Xie <david.scriptfan@gmail.com>
2016-01-17 19:25:49 +0800

    模型重构的第一步: 将Code改成内嵌文档

--- daimaduan/models/rate.py
+++
@@ -1,9 +0,0 @@
-import mongoengine
-
-from daimaduan.models import BaseDocument, User, Paste
-
-
-class Rate(BaseDocument):
-    user = mongoengine.ReferenceField(User)
-    paste = mongoengine.ReferenceField(Paste)
-    score = mongoengine.IntField(default=0)
