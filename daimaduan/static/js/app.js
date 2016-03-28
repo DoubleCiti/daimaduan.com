2993adf30e940a9b1d6b37830e18437ee9789c82
David Xie <david.scriptfan@gmail.com>
2016-03-28 16:11:45 +0800

    增加了将代码段移出收藏夹的功能
    至此, 收藏夹功能到一个段落了

--- daimaduan/static/js/app.js
+++ daimaduan/static/js/app.js
@@ -10,26 +10,6 @@ function signoutHandler() {
   });
 }
 
-function createLi(paste) {
-      var li = '<li class="list-group-item">';
-      li += '<div class="pull-right">';
-      li += '<a href="/user/' + paste.user.username + '" class="text-muted" title="' + paste.user.username + '">';
-      li += '<img src="' + paste.user.gravatar_url + '" alt="' + paste.user.username + '" class="img-rounded"/></a>';
-      li += '</div>';
-      li += '<h5>';
-      li += '<a href="/paste/' + paste.hash_id + '" title="' + paste.title + '">' + paste.title + '</a> ';
-      li += '<span class="label label-default">' + paste.time_passed + '</span> ';
-      li += '<span class="label label-primary">有' + paste.num_codes + '段代码</span>';
-      li += '<a href="/paste/' + paste.hash_id + '#disqus_thread" class="label label-info"></a>'
-      li += '</h5>';
-      $.each(paste.tags, function(i, tag) {
-        li += '<a href="/tag/' + tag + '" class="btn btn-xs btn-warning" title="' + tag + '">' + tag + '</a>';
-      });
-      li += '<div class="clearfix"></div>';
-      li += '</li>';
-      return li;
- }
-
   app.faIcon = function(icon, text) {
     var template = _.template('<i class="fa fa-<%= icon %>"></i> <%= text %>');
     return template({ icon: icon, text: text });
