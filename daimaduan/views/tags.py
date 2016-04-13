97bea3bfee4a3a651343b5d9a8667459cf1ae9fb
David Xie <david.scriptfan@gmail.com>
2016-04-13 21:20:40 +0800

    给标签查看页面加入了"只看我的"选项 #250

--- daimaduan/views/tags.py
+++ daimaduan/views/tags.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request
+from flask.ext.login import current_user
 
 from daimaduan.models.base import Paste
 from daimaduan.models.tag import Tag
@@ -14,7 +16,10 @@ def view(tag_name):
     tag = Tag.objects.get_or_404(key=tag_name)
     page = get_page()
 
-    pastes = tag.pastes(is_private=False).order_by('-updated_at')
+    if request.args.get('filter', None) == 'mine' and current_user.is_authenticated:
+        pastes = tag.pastes(user=current_user.user).order_by('-updated_at')
+    else:
+        pastes = tag.pastes(is_private=False).order_by('-updated_at')
     pagination = pastes.paginate(page, per_page=20)
 
     return render_template('tags/view.html',
